bdb5e30dc7650bd150d7ddb0c7a10babb0ab5f5f
faishalam <faishalabdulmajid@gmail.com>
2024-11-11 18:40:32 +0700

    done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className="max-w-full w-full min-h-screen h-full mx-auto"
+        // className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black`}
       >
         {children}
       </body>
